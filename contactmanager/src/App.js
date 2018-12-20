4e77073212e7324ad01f11453ffe97e6db5b2baa
Chen Yujing <vit7david@outlook.com>
2018-12-20 11:40:39 +0200

    testing JSX

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
+    const name = "John";
+    const showHello = false;
+    const showMath = true;
+    const num1 = 40;
+    const num2 = 23;
+
+    let math;
+    if (showMath) {
+      math = (
+        <h4>
+          {num1}+{num2} = {num1 + num2}
+        </h4>
+      );
+    } else {
+      math = null;
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>The App Component</h1>
+        {showHello ? <h4>Hello {name.toUpperCase()} </h4> : null}
+        {math}
       </div>
     );
   }
