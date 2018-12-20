c90642b50016725874eac91f44e66673392dcffa
Chen Yujing <vit7david@outlook.com>
2018-12-20 17:10:33 +0200

    first react component

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,29 +1,13 @@
 import React, { Component } from "react";
-
+import "./App.css";
+import Contact from "./components/Contact";
+import "./App.css";
 class App extends Component {
   render() {
-    const name = "John";
-    const showHello = false;
-    const showMath = true;
-    const num1 = 40;
-    const num2 = 23;
-
-    let math;
-    if (showMath) {
-      math = (
-        <h4>
-          {num1}+{num2} = {num1 + num2}
-        </h4>
-      );
-    } else {
-      math = null;
-    }
-
     return (
       <div className="App">
         <h1>The App Component</h1>
-        {showHello ? <h4>Hello {name.toUpperCase()} </h4> : null}
-        {math}
+        <Contact />
       </div>
     );
   }
---
+++ contactmanager/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>David water</h4>
+        <ul>
+          <li>Email: Dwater@gmail.com</li>
+          <li>Phone: 1234-1234-1234</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
