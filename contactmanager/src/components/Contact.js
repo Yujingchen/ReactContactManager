1d7c87b94726ec0b14a8120aa09fbd1ab602fc97
Chen Yujing <vit7david@outlook.com>
2018-12-21 16:25:34 +0200

    add event to show and hide contact

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -2,15 +2,38 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./contact.css";
 class Contact extends Component {
+  state = {
+    showContactInfo: true
+  };
+
+  onShowClick = e => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  };
+
   render() {
     const { name, email, phone } = this.props.contact; //destructuring
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        <h4>
+          {name}{" "}
+          <i
+            onClick={() =>
+              this.setState({
+                showContactInfo: !this.state.showContactInfo
+              })
+            }
+            className="fas fa-sort-down"
+          />
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
