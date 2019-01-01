b1b36391d480a9cc572990326470f44bc2f26e48
Chen Yujing <vit7david@outlook.com>
2019-01-01 16:31:08 +0200

    use react router to add more pages to my contact manager application

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./components/contacts/Contacts";
 import Header from "./components/layout/Header";
+import About from "./components/pages/About";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "./context";
@@ -10,16 +12,21 @@ import AddContact from "./components/contacts/AddContact";
 class App extends Component {
   render() {
     return (
-      <Provider>
-        <div className="App">
-          <Header branding="Contact Manager" test="1.0" />
+      <Router>
+        <Provider>
+          <div className="App">
+            <Header branding="Contact Manager" test="1.0" />
 
-          <div className="container">
-            <AddContact />
-            <Contacts />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Provider>
+        </Provider>
+      </Router>
     );
   }
 }
