5e36d4b604b03d3b1f5ea839ea2d8d65d6e50768
Chen Yujing <vit7david@outlook.com>
2019-01-02 22:45:05 +0200

    Use fake API to fetch data for contacts

--- contactmanager/src/components/test/test.js
+++ contactmanager/src/components/test/test.js
@@ -4,36 +4,48 @@ import React, { Component } from "react";
 
 class Test extends Component {
   state = {
-    test: "test"
+    title: "",
+    body: ""
   };
   componentDidMount() {
-    console.log("componentDidMount....");
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(response => response.json())
+      //fetch return a promise then it is parse into json
+      .then(data =>
+        //work with JSON data here
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      ); //.catch(err=>{}) Do something for an error;
   }
-  componentWillMount() {
-    console.log("componentWillMount....");
-  }
+  //   componentWillMount() {
+  //     console.log("componentWillMount....");
+  //   }
 
-  componentDidUpdate() {
-    console.log("componentDidUpdate....");
-  }
-  componentWillUpdate() {
-    console.log("componentWillUpdate....");
-  }
-  componentWillReceiveProps(nextProps, nextState) {
-    console.log("componentWillReceiveProps...");
-  }
+  //   componentDidUpdate() {
+  //     console.log("componentDidUpdate....");
+  //   }
+  //   componentWillUpdate() {
+  //     console.log("componentWillUpdate....");
+  //   }
+  //   componentWillReceiveProps(nextProps, nextState) {
+  //     console.log("componentWillReceiveProps...");
+  //   }
 
-  static getDervivedStateFromProps(nextProps, prevState) {
-    return null;
-  }
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log("getSnapshotBeforeUpdate...");
-  }
+  //   static getDervivedStateFromProps(nextProps, prevState) {
+  //     return null;
+  //   }
+  //   getSnapshotBeforeUpdate(prevProps, prevState) {
+  //     console.log("getSnapshotBeforeUpdate...");
+  //   }
 
   render() {
+    const { title, body } = this.state;
     return (
       <div>
-        <h1>Test component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
--- contactmanager/src/context.js
+++ contactmanager/src/context.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 const Context = React.createContext();
 
 const reducer = (state, action) => {
@@ -24,30 +24,27 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: "David Water",
-        email: "dwater@gamil.com",
-        phone: "1234-1234-1234"
-      },
-      {
-        id: 2,
-        name: "John Nelson",
-        email: "Jnelson@gamil.com",
-        phone: "3333-3333-3333"
-      },
-      {
-        id: 3,
-        name: "Jack Lawson",
-        email: "Jlawson@gamil.com",
-        phone: "777-777-777"
-      }
-    ],
+    contacts: [],
     dispatch: action => {
       this.setState(state => reducer(state, action));
     }
   };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(response =>
+      this.setState({
+        contacts: response.data
+      })
+    );
+
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then(response => response.json())
+    //   .then(data =>
+    //     this.setState({
+    //       contacts: data
+    //     })
+    //   );
+  }
 
   render() {
     return (
